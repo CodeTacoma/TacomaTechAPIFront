9ae82a10fc41717c8fd84214b434bc4aba1b2d85
git_dave <ctchapman@gmail.com>
2018-08-01 15:49:10 -0700

    app render method now set to new code

--- src/App.js
+++ src/App.js
@@ -121,45 +121,45 @@ getBizListings = async () => {
 };
 
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
 
 
 /////////////////////////////////////////////////////////////////////////////
 
-// render() {
-//   const { cityListings, bizListings, eduListings, eventListings, groupListings } = this.state;
+render() {
+  const { cityListings, bizListings, eduListings, eventListings, groupListings } = this.state;
 
-//   return (
+  return (
 
-//       <div className="App">
-//           <HeaderAppBar />
-//           <h1>Tacoma's Tech Ecosystem</h1>
-//           {/* Render the listings if we have them */}
-//           <LandingExpansionPanel
-//               bizList={ bizListings }
-//               cityList={ cityListings }
-//               eduList={ eduListings }
-//               eventList={ eventListings }
-//               groupList={ groupListings }
-//           />
-//       </div>
-//   );
-// }
-// }
+      <div className="App">
+          <HeaderAppBar />
+          <h1>Tacoma's Tech Ecosystem</h1>
+          {/* Render the listings if we have them */}
+          <LandingExpansionPanel
+              bizList={ bizListings }
+              cityList={ cityListings }
+              eduList={ eduListings }
+              eventList={ eventListings }
+              groupList={ groupListings }
+          />
+      </div>
+  );
+}
+}
 
-// export default App;
+export default App;
