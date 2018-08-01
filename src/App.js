434e465fdb7ed2365a422a825aac968ba66392e9
git_dave <ctchapman@gmail.com>
2018-08-01 12:01:05 -0700

    added in lifecycle and fetching methods to App in app.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,108 @@ import { BrowserRouter as Router, Link } from 'react-router-dom'
 import LandingExpansionPanel from './components/LandingExpansionPanel'
 import HeaderAppBar from "./components/HeaderAppBarClass";
 
-//import React, { Component } from 'react';
 import logo from './logo.svg';
-//import './App.css';
+
 
 class App extends Component {
+
+//   componentDidMount() {
+//     //this.getListings();
+//     this.getBizListings();
+//     this.getCityListings();
+//     this.getEduListings();
+//     this.getEventListings();
+//     this.getTechGroupListings();
+// }
+
+componentDidUpdate() {
+    console.log("App updated");
+}
+
+/*
+    Load category info for the "landing page" render the user will see first, also
+    serves as the "home page" for the app.
+    Much thanks to Gavin for getting me straightened out here and getting the listings []
+    loaded properly!
+ */
+getListings = async () => {
+    // Get the listings and store them in state
+    try {
+        const data = await fetch('/api');
+        const prettyData = await data.json();
+        //console.log(prettyData.roots);
+        this.setState({ listings: prettyData.roots })
+    }
+    catch (error) {
+        console.log('error', error);
+    }
+};
+
+getTechGroupListings = async () => {
+    // Get the listings and store them in state
+    try {
+        const data = await fetch('/groups');
+        const prettyData = await data.json();
+        //console.log(prettyData.roots);
+        this.setState({ groupListings: prettyData.groupListings })
+    }
+    catch (error) {
+        console.log('error', error);
+    }
+};
+
+getEventListings = async () => {
+    // Get the listings and store them in state
+    try {
+        const data = await fetch('/events');
+        const prettyData = await data.json();
+        //console.log(prettyData.roots);
+        this.setState({ eventListings: prettyData.eventsListings })
+    }
+    catch (error) {
+        console.log('error', error);
+    }
+};
+
+getEduListings = async () => {
+    // Get the listings and store them in state
+    try {
+        const data = await fetch('/education');
+        const prettyData = await data.json();
+        //console.log(prettyData.roots);
+        this.setState({ eduListings: prettyData.edulisting })
+    }
+    catch (error) {
+        console.log('error', error);
+    }
+};
+
+getCityListings = async () => {
+    // Get the listings and store them in state
+    try {
+        const data = await fetch('/city');
+        const prettyData = await data.json();
+        //console.log(prettyData.roots);
+        this.setState({ cityListings: prettyData.cityListings })
+    }
+    catch (error) {
+        console.log('error', error);
+    }
+};
+
+getBizListings = async () => {
+    try {
+        const data = await fetch('/business');
+        const prettyData = await data.json();
+        //console.log(prettyData.bizListings);
+        this.setState({ bizListings: prettyData.bizListings })
+    }
+    catch (error) {
+        console.log('error', error);
+    }
+};
+
+
   render() {
     return (
       <div className="App">
