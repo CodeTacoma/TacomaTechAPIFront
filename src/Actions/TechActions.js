53a9373cf6ff4e132199a8b9757cfa8234f10fbb
GavinThomas1192 <gthomas1192@gmail.com>
2018-08-03 07:37:05 -0700

    fixed styles, and added local storage caching

--- src/Actions/TechActions.js
+++ src/Actions/TechActions.js
@@ -3,14 +3,18 @@ export const listingSet = (type, listings) => ({type: `${type}_SET`, payload: li
 
 
 export const fetchAllListings = () => dispatch => new Promise ( async (resolve, reject)  => {
-
-
     const routes = ['business', 'groups', 'events', 'education', 'city']
     try  {
         Promise.all(routes.map( async (ele, index) => {
-            let data = await fetch(`${root}/${ele}`);
-            const prettyData = await data.json();
-            dispatch(listingSet(ele, prettyData))
+            const localStorageData = await localStorage.getItem(ele)
+            if (!!localStorageData ) {
+                dispatch(listingSet(ele, JSON.parse(localStorageData)))
+            } else {
+                let data = await fetch(`${root}/${ele}`);
+                const prettyData = await data.json();
+                dispatch(listingSet(ele, prettyData))
+                localStorage.setItem(ele, JSON.stringify(prettyData))
+            }
         })).then(() => resolve());
     }
     catch(error) {
